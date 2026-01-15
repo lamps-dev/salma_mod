c9dee8a3c565e84653c051087b9da4d425852100
6em-dev <rimit58872@gmail.com>
2026-01-15 16:54:21 +0100

    New block: Salma block

--- src/main/java/net/lampsdev/salmamod/SalmaMod.java
+++ src/main/java/net/lampsdev/salmamod/SalmaMod.java
@@ -2,6 +2,7 @@ package net.lampsdev.salmamod;
 
 import net.fabricmc.api.ModInitializer;
 
+import net.lampsdev.salmamod.block.ModBlocks;
 import net.lampsdev.salmamod.item.ModItems;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -13,5 +14,6 @@ public class SalmaMod implements ModInitializer {
 	@Override
 	public void onInitialize() {
 		ModItems.registerModItems();
+		ModBlocks.registerModBlocks();
 	}
 }
--- src/main/java/net/lampsdev/salmamod/block/ModBlocks.java
+++ src/main/java/net/lampsdev/salmamod/block/ModBlocks.java
@@ -1,4 +1,54 @@
 package net.lampsdev.salmamod.block;
 
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+import net.lampsdev.salmamod.SalmaMod;
+import net.minecraft.block.AbstractBlock;
+import net.minecraft.block.Block;
+import net.minecraft.item.BlockItem;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroups;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.registry.RegistryKey;
+import net.minecraft.registry.RegistryKeys;
+import net.minecraft.sound.BlockSoundGroup;
+import net.minecraft.util.Identifier;
+
 public class ModBlocks {
+    public static final Block SALMA_BLOCK = registerBlock("salma_block",
+            AbstractBlock.Settings.create().strength(0.7f).sounds(BlockSoundGroup.WOOL).burnable());
+
+
+    private static void registerBlockItem(String name, Block block) {
+
+        RegistryKey<Item> key = RegistryKey.of(RegistryKeys.ITEM, Identifier.of(SalmaMod.MOD_ID, name));
+
+        BlockItem item = new BlockItem(block, new Item.Settings().registryKey(key));
+
+        Registry.register(Registries.ITEM, key, item);
+
+        SalmaMod.LOGGER.info("Registered BlockItem: " + name); // ADD THIS LINE
+
+    }
+
+    private static Block registerBlock(String name, AbstractBlock.Settings blockSettings) {
+
+        RegistryKey<Block> key = RegistryKey.of(RegistryKeys.BLOCK, Identifier.of(SalmaMod.MOD_ID, name));
+
+        Block block = new Block(blockSettings.registryKey(key));
+
+        registerBlockItem(name, block);
+
+        return Registry.register(Registries.BLOCK, key, block);
+
+    }
+
+
+    public static void registerModBlocks() {
+        SalmaMod.LOGGER.info("Registering mod blocks for" + SalmaMod.MOD_ID);
+
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.BUILDING_BLOCKS).register(entries -> {
+            entries.add(ModBlocks.SALMA_BLOCK);
+        });
+    }
 }
